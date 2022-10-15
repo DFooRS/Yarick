552b86864ae3bad2a4fd5a2983a7acb356c72536
DFooRS <dima.lutsenko03@gmail.com>
2022-10-15 12:18:24 +0300

    убрал пресс ентер

--- arithemetic.py
+++ arithemetic.py
@@ -1,4 +1,3 @@
 print("Solve an example 4 * 100 - 54")
 ans = input()
 print("Right answer is 346. Your answer is " + ans)
-input('Press ENTER to exit')
--- individual.py
+++ individual.py
@@ -1,4 +1,3 @@
 mass = float(input("Enter mass (in kg): "))
 volume = float(input("Enter volume(in m^3): "))
 print("Density is ", mass / volume, "kg/m^3")
-input("Press ENTER to exit")
